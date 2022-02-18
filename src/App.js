4d6b91a034958db177b1f56f60312447e5fbe215
Beth Sutton <bksutton2@gmail.com>
2022-02-18 10:47:01 -0700

    added categories for explore page, category page, added fetch listings from firebase

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import Category from './pages/Category';
 
 function App() {
   return (
@@ -19,6 +20,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          {/* :categoryName NAME FOR PARAMS IN CATEGORY PAGE */}
+          <Route path="/category/:categoryName" element={<Category />} />
 
           {/* PRIVATE ROUTE STARTS HERE */}
           <Route path="/profile" element={<PrivateRoute />}>
