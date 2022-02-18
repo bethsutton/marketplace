f70d024418e5a99ecfa3d1662b0d097a8f51ec14
Beth Sutton <bksutton2@gmail.com>
2022-02-17 17:19:57 -0700

    added private route for profile page, added useAuthStatus hook, added spinner component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // COMPONENTS
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 // PAGES
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
@@ -18,7 +19,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
-          <Route path="/profile" element={<Profile />} />
+
+          {/* PRIVATE ROUTE STARTS HERE */}
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
